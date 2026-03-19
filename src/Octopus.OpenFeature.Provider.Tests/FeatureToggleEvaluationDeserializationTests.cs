eb1b5b83a33f07d51a515d2732962d217eb34cd9
Liam Hughes <liam.hughes@octopus.com>
2026-03-19 13:38:02 +1100

    JSON deserialisation tests

---
+++ src/Octopus.OpenFeature.Provider.Tests/FeatureToggleEvaluationDeserializationTests.cs
@@ -0,0 +1,80 @@
+using System.Text.Json;
+using FluentAssertions;
+
+namespace Octopus.OpenFeature.Provider.Tests;
+
+public class FeatureToggleEvaluationDeserializationTests
+{
+    [Fact]
+    public void ShouldDeserializeEnabledToggle()
+    {
+        var json = """
+                   {"name":"My Feature","slug":"my-feature","isEnabled":true,"segments":[]}
+                   """;
+
+        var result = JsonSerializer.Deserialize<FeatureToggleEvaluation>(json, JsonSerializerOptions.Web);
+
+        result!.Name.Should().Be("My Feature");
+        result.Slug.Should().Be("my-feature");
+        result.IsEnabled.Should().BeTrue();
+        result.Segments.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void ShouldDeserializeDisabledToggle()
+    {
+        var json = """
+                   {"name":"My Feature","slug":"my-feature","isEnabled":false,"segments":[]}
+                   """;
+
+        var result = JsonSerializer.Deserialize<FeatureToggleEvaluation>(json, JsonSerializerOptions.Web);
+
+        result!.IsEnabled.Should().BeFalse();
+    }
+
+    [Fact]
+    public void ShouldDeserializeToggleWithMissingSegmentsFieldAsNull()
+    {
+        var json = """
+                   {"name":"My Feature","slug":"my-feature","isEnabled":true}
+                   """;
+
+        var result = JsonSerializer.Deserialize<FeatureToggleEvaluation>(json, JsonSerializerOptions.Web);
+
+        result!.Segments.Should().BeNull();
+    }
+
+    [Fact]
+    public void ShouldDeserializeToggleWithSegments()
+    {
+        var json = """
+                   {"name":"My Feature","slug":"my-feature","isEnabled":true,"segments":[{"key":"license-type","value":"free"},{"key":"country","value":"au"}]}
+                   """;
+
+        var result = JsonSerializer.Deserialize<FeatureToggleEvaluation>(json, JsonSerializerOptions.Web);
+
+        result!.Segments.Should().HaveCount(2).And.Contain([
+            new KeyValuePair<string, string>("license-type", "free"),
+            new KeyValuePair<string, string>("country", "au")
+        ]);
+    }
+
+    [Fact]
+    public void ShouldDeserializeArrayOfToggles()
+    {
+        var json = """
+                   [
+                     {"name":"Feature A","slug":"feature-a","isEnabled":true,"segments":[]},
+                     {"name":"Feature B","slug":"feature-b","isEnabled":false,"segments":[]}
+                   ]
+                   """;
+
+        var result = JsonSerializer.Deserialize<FeatureToggleEvaluation[]>(json, JsonSerializerOptions.Web);
+
+        result.Should().HaveCount(2);
+        result![0].Slug.Should().Be("feature-a");
+        result[0].IsEnabled.Should().BeTrue();
+        result[1].Slug.Should().Be("feature-b");
+        result[1].IsEnabled.Should().BeFalse();
+    }
+}
