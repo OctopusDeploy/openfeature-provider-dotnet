fc924e19abb31f5d78bfc7fbb96c638252ad3930
Liam Hughes <liam.hughes@octopus.com>
2026-08-06 14:58:01 +1000

    Move EvaluationResponse to own file

--- src/Octopus.OpenFeature.Provider/FeatureFlagApiClient.cs
+++ src/Octopus.OpenFeature.Provider/FeatureFlagApiClient.cs
@@ -6,13 +6,6 @@ using Octopus.OpenFeature.Provider.V4;
 
 namespace Octopus.OpenFeature.Provider;
 
-internal class EvaluationResponse(ServerSideEvaluation[] evaluations, byte[] contentHash)
-{
-    public ServerSideEvaluation[] Evaluations { get; } = evaluations;
-
-    public byte[] ContentHash { get; } = contentHash;
-}
-
 internal interface IFeatureFlagApiClient
 {
     Task<bool> HaveFeaturesChanged(byte[] contentHash, CancellationToken cancellationToken);
